741523905cca7dfa0f616e6b7bf5ce46b082e104
Abel Misiocha <wells.mail.13@gmail.com>
2024-03-11 02:24:12 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata, Viewport } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "The Writer's Palace",
+  description:
+    'Welcome to WritersPalace, Discover authentic tools and services tailored to elevate your craft. Unleash your creativity and join a vibrant community of wordsmiths. Your literary journey begins here.',
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  category: 'Ecommerce Website',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
