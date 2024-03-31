8f392a7e4441ce46e44b9386b78a8bf1cb5c73ba
Codedwells <wells.mail.13@gmail.com>
2024-03-31 19:24:46 +0300

    feat(core): Add contact page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,10 @@ export default function Page() {
 	return (
 		<div className='mx-64 flex h-full flex-col items-center py-14'>
 			<h1 className='mb-4 text-3xl font-semibold text-gray-900 dark:text-slate-200 md:text-4xl'>
-				AI-Enhanced, User-Friendly Dictionary
+				About
 			</h1>
 
+			<h2 className='mt-8 text-2xl font-semibold'>A Community Project, User-Friendly Dictionary</h2>
 			<p className='max-w-lg text-center'>
 				SimplerDictionary, powered by AI, uses SimplerLLM and GPT-4 for
 				clear, easy-to-understand word definitions. It also includes
@@ -13,14 +14,14 @@ export default function Page() {
 			</p>
 
 			<h2 className='mt-14 text-2xl font-semibold'>A Community Project</h2>
-			<p className='max-w-lg text-center'>
+			<p className='max-w-xl text-center'>
 				This dictionary is not just a tool, but a community project.
 				It's open-source, so anyone can add, change, or explore new
 				ideas. It's a creative space for all.
 			</p>
 			<ul className='mt-4 list-inside list-disc space-y-2'>
 				<p className='text-lg font-[500]'>Built with care:</p>
-				<li className='max-w-md'>
+				<li className='max-w-xl'>
 					Our strong backend uses Python FastAPI. Check out our
 					backend on GitHub: SimplerDictionary-API URL
 				</li>
@@ -32,7 +33,7 @@ export default function Page() {
 			</ul>
 
 			<h2 className='mt-14 text-2xl font-semibold'>Rich Data Resource</h2>
-			<p className='max-w-lg text-center'>
+			<p className='max-w-xl text-center'>
 				We offer our full dataset for anyone interested, from
 				researchers to developers. It's a valuable resource for many
 				uses. Download it here: [Link to Dataset].
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,30 @@
 export default function Page() {
 	return (
-		<div>
-			<p>This is the contact page</p>
+		<div className='mx-64 flex h-full flex-col items-center justify-center py-14'>
+			<h1 className='mb-4 flex items-center gap-2 text-3xl font-semibold text-gray-900 dark:text-slate-200 md:text-4xl'>
+				Contact Us
+			</h1>
+			<h2 className='text-lg'>
+				Have questions, suggestions, or feedback? We're here to listen.
+			</h2>
+			<h2 className='mt-10 text-xl font-semibold'>Reach Out to Us</h2>
+			<p className='mt-1 max-w-lg text-center'>
+				Your thoughts and insights are important to us. If there’s
+				anything you'd like to share or inquire about, please don't
+				hesitate to reach out.
+			</p>
+
+			<p className='mt-8 max-w-lg text-center'>
+				You can email <b>Hasan</b>, the founder of&nbsp;
+				<b>SimplerDictionary</b> directly at &nbsp;
+				<a
+					target='_blank'
+					href='mailto:hasan@learnwithhasan.com'
+					className='text-blue-600 hover:text-blue-500'
+				>
+					hasan@learnwithhasan.com
+				</a>
+			</p>
 		</div>
 	)
 }
